86c2c8c735cc18744e467366f68289cdbfb0a4de
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2018-02-11 20:46:17 +0530

    Revamped profile view and added account activation

--- portal/forms.py
+++ portal/forms.py
@@ -1,48 +1,33 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .models import Tournament,Team,Profile
+from portal.models import Tournament, Team, Profile
 from django.forms import Form, ModelForm, TextInput
 
-class UserForm(ModelForm):
-    password = forms.CharField(label='password', required=True, widget=forms.PasswordInput(
-        attrs={'placeholder': 'Password'}))
-    
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
+    subscribe = forms.BooleanField(required=False)
+    agree_terms = forms.BooleanField(required=True)
+
     class Meta:
         model = User
-        password = forms.CharField(label='password', required=True, widget=forms.PasswordInput(
-            attrs={'placeholder': 'Password'}))
-        fields = ['username','email','password','first_name','last_name']
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'subscribe', 'agree_terms')
         widgets = {
             'username': TextInput(attrs={'placeholder': 'Username'}),
             'email': TextInput(attrs={'placeholder': 'Email'}),
             'first_name': TextInput(attrs={'placeholder': 'First name'}),
-            'last_name': TextInput(attrs={'placeholder': 'Last name'}) ,
+            'last_name': TextInput(attrs={'placeholder': 'Last name'}),
         }
-        labels = {
-                'email' : 'email',
-                'password': 'password',
-            }
 
-class UpdateForm(Form):
-    username = forms.CharField(label='username', max_length=100, required=False, widget=forms.TextInput(
-        attrs={'placeholder': 'Username', 'class': 'form-control'}))
-
-
-class TournamentForm(ModelForm):   
+class UserForm(forms.ModelForm):
     class Meta:
-        model = Tournament
-        fields = ['tournament_name','tournament_date','no_of_players']
-        
-class subscriptionform(Form):
-    is_subscribe = forms.BooleanField(initial=True)
+        model = User
+        fields = ('username',)
 
 class ProfileForm(Form):
-    steam_id = forms.CharField(label='steam_id', max_length=100, required=False, widget=forms.TextInput(attrs={'placeholder': 'Steam ID', 'class':'form-control'}))
-    location = forms.CharField(label='location', max_length=100, required=False, widget=forms.TextInput(attrs={'placeholder': 'Location', 'class':'form-control'}))
-
-class changepass(Form):
-    new_pass = forms.CharField(label='New Password', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'New Password', 'class':'form-control'}))
+    class Meta:
+        model = Profile
+        fields = ('location', 'user_avatar')
 
 class forgetpass(Form):
     usernamee = forms.CharField(label='Username', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'enter username', 'class':'form-control'}))
