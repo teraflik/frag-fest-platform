44cdd8f75f08b7442d65fda315340b1b7221ef42
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2018-02-19 23:18:27 +0530

    Social link sidebar minor update

--- portal/static/portal/js/main.js
+++ portal/static/portal/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $("#side-toggle").click(function () {
-		$("#side-float").toggleClass("visible");
+      $("#side-float").toggleClass("visible");
+      $("#side-toggle i").toggleClass("fa-chevron-circle-left fa-chevron-circle-right");
+      return false;
     });
 });
