2658b9bd17e34df0a4f16535e019954faeb3dff0
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2018-02-20 18:31:10 +0530

    Add drop-down for games

--- portal/static/portal/js/main.js
+++ portal/static/portal/js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
       $("#side-toggle i").toggleClass("fa-chevron-circle-left fa-chevron-circle-right");
       return false;
     });
+    $('body').on('mouseenter mouseleave','.dropdown',function(e){
+      var _d=$(e.target).closest('.dropdown');_d.addClass('show');
+      setTimeout(function(){
+        _d[_d.is(':hover')?'addClass':'removeClass']('show');
+      },300);
+    });
 });
